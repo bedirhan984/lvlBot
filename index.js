95ea8846b3c17a61528a4eee90f9de3c91546551
bedirhan984 <64159786+bedirhan984@users.noreply.github.com>
2026-01-28 20:10:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -112,4 +112,4 @@ bot.on('message', (message) => {
     }
 });
 
-bot.login('not process.env.token'); // Makes the bot login to the token in the env file.
+bot.login('MTE5MzYwNTQ5MTYyOTI0MDM5MQ.GlsBYg.rLQUlJzWrDejbxoug1j7KYpSYDpz53dYZZp5Ks'); // Makes the bot login to the token in the env file.
